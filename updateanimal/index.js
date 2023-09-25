7fd4aa10249b684f51e8b4d58f3b69cb7fdbbbdf
QueAnka <annika.nylin@hotmail.com>
2023-09-25 22:05:34 +0200

    added update-function

---
+++ updateanimal/index.js
@@ -0,0 +1,48 @@
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import {DynamoDBDocumentClient,PutCommand} from "@aws-sdk/lib-dynamodb";
+
+
+const client = new DynamoDBClient({});
+const dynamo = DynamoDBDocumentClient.from(client);
+
+const tableName = "animals-table";
+
+export const handler = async (event) => {
+  let body;
+  let statusCode = 200;
+  const headers = {
+    "Content-Type": "application/json",
+  };
+
+  try {
+    
+
+    
+    const requestData = JSON.parse(event.body);
+    
+      // Skapa en PUTCommand för att ändra Item
+      await dynamo.send(
+        new PutCommand({
+          TableName: tableName,
+          Item: {
+			id: requestData.id,
+            animal: requestData.animal
+          },
+        })
+      );
+
+      body = `Addedd item ${requestData.id}`;
+    } 
+   catch (error) {
+    statusCode = 500;
+    body = error.message;
+  } finally {
+    body = JSON.stringify(body);
+  }
+
+  return {
+    statusCode,
+    body,
+    headers,
+  };
+};
